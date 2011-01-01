45658f194d9bd4702e82588915b20ed5de1bea03
Manuel <manue.l@live.com.ar>
2011-01-01 12:22:28 -0300

    Core/Battleground: Improved code of commit d7261e4689f57d2fd5bd
    Thank you Machiavelli
    
    Signed-off-by: Manuel <manue.l@live.com.ar>

--- src/server/game/Battlegrounds/Battleground.cpp
+++ src/server/game/Battlegrounds/Battleground.cpp
@@ -1427,16 +1427,8 @@ void Battleground::RemovePlayerFromResurrectQueue(uint64 player_guid)
 
 bool Battleground::AddObject(uint32 type, uint32 entry, float x, float y, float z, float o, float rotation0, float rotation1, float rotation2, float rotation3, uint32 /*respawnTime*/)
 {
-    // if it is something new
-    if (!m_BgObjects[type])
-    {
-        if (m_BgObjects.size()+1 > m_BgObjects.capacity())
-        {
-            // should we use an assert?
-            sLog->outError("Battleground::AddObject: Failed spawning gameobject (type %u). The vector needs more capacity, you must resize it!",type);
-            return false;
-        }
-    }
+    // If the assert is called, means that m_BgObjects must be resized!
+    ASSERT(type < m_BgObjects.size());
 
     Map *map = GetBgMap();
     if (!map)
@@ -1564,16 +1556,8 @@ void Battleground::SpawnBGObject(uint32 type, uint32 respawntime)
 
 Creature* Battleground::AddCreature(uint32 entry, uint32 type, uint32 teamval, float x, float y, float z, float o, uint32 respawntime)
 {
-    // if it is something new
-    if (!m_BgCreatures[type])
-    {
-        if (m_BgCreatures.size()+1 > m_BgCreatures.capacity())
-        {
-            // should we use an assert?
-            sLog->outError("Battleground::AddCreature: Failed spawning creature (type %u). The vector needs more capacity, you must resize it!",type);
-            return NULL;
-        }
-    }
+    // If the assert is called, means that m_BgCreatures must be resized!
+    ASSERT(type < m_BgCreatures.size());
 
     Map * map = GetBgMap();
     if (!map)
